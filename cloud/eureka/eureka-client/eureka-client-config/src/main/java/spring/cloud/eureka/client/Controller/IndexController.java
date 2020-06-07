a478819952ef39b0a8c0c02640d785391ddcd40b
zhuleiming <zhuleiming03@gmail.com>
2020-06-07 15:20:51 +0800

    init

---
+++ cloud/eureka/eureka-client/eureka-client-config/src/main/java/spring/cloud/eureka/client/Controller/IndexController.java
@@ -0,0 +1,25 @@
+package spring.cloud.eureka.client.Controller;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+
+@RestController
+public class IndexController {
+
+    @Resource
+    private DiscoveryClient client;
+
+    @RequestMapping("index")
+    public String hello() {
+
+        // 打印出所有的服务名
+        System.out.printf("------ %s 当前获取获取如下服务 -----\n",
+                LocalDateTime.now());
+        client.getServices().forEach(System.out::println);
+        return "Hello world";
+    }
+}
---
+++ cloud/eureka/eureka-client/eureka-client-config/src/main/java/spring/cloud/eureka/client/EurekaClientConfigApplication.java
@@ -0,0 +1,15 @@
+package spring.cloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientConfigApplication.class, args);
+    }
+
+}
