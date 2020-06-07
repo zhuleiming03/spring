61a0067feeb63c3efec48f6cc8a52bd6a2f20a6c
zhuleiming <zhuleiming03@gmail.com>
2020-06-07 11:03:26 +0800

    init

---
+++ cloud/eureka/eureka-service/eureka-service-cluster/src/main/java/spirng/cloud/eureka/service/EurekaServiceClusterApplication.java
@@ -0,0 +1,23 @@
+package spirng.cloud.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心（集群）
+ * 需要配置主机域名
+ * Windows:C:\Windows\System32\drivers\etc\hosts 添加 127.0.0.1 peer1 127.0.0.1 peer2
+ * 打包好后分别运行
+ * java -jar eureka-service-cluster-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer1
+ * java -jar eureka-service-cluster-0.0.1-SNAPSHOT.jar --spring.profiles.active=peer2
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceClusterApplication.class, args);
+    }
+
+}
---
+++ cloud/eureka/eureka-service/eureka-service-cluster/src/test/java/spirng/cloud/eureka/service/EurekaServiceClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package spirng.cloud.eureka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
