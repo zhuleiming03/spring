fb49e20b5bd2141e86b9a77c6268b52cbe58d9ae
zhuleiming <zhuleiming03@gmail.com>
2020-06-07 14:45:15 +0800

    init

---
+++ cloud/eureka/eureka-client/eureka-client-base/src/test/java/spring/cloud/eureka/client/EurekaClientBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.cloud.eureka.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
