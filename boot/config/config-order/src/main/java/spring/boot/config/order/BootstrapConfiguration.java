905c388a081ce8a24102c5fd082dec5ad1acc70c
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2020-06-09 13:19:01 +0800

    del

--- boot/config/config-order/src/main/java/spring/boot/config/order/BootstrapConfiguration.java
+++
@@ -1,9 +0,0 @@
-package spring.boot.config.order;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-
-@Configuration
-@PropertySource("classpath:config/service.properties")
-public class BootstrapConfiguration {
-}
