1be3b8fed087a34a8de001916dbba6adf07e5ccf
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2023-06-30 14:33:13 +0800

    reset

--- core/Actuator/src/main/java/demo/ActuatorApplication.java
+++ core/Actuator/src/main/java/demo/ActuatorApplication.java
@@ -1,4 +1,4 @@
-package spring.core.actuator;
+package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- core/Actuator/src/test/java/demo/ActuatorApplicationTests.java
+++ core/Actuator/src/test/java/demo/ActuatorApplicationTests.java
@@ -1,4 +1,4 @@
-package spring.core.actuator;
+package demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
