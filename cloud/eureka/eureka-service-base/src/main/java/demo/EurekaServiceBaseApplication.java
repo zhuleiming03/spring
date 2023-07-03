57c395de297af4b22859cbf3caf0657db9d91312
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2023-07-03 15:06:31 +0800

    reset

---
+++ cloud/eureka/eureka-service-base/src/main/java/demo/EurekaServiceBaseApplication.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 服务注册中心（单点）
+ * 运行后 访问 http://localhost:8010/
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceBaseApplication.class, args);
+    }
+
+}
