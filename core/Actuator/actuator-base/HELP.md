0bb62e628e7ba89e110d6ea1c3a03f012df84a6e
zhuleiming03@gmail.com <zhuleiming03@gmail.com>
2023-07-04 16:39:44 +0800

    reset

---
+++ core/Actuator/actuator-base/HELP.md
@@ -0,0 +1,2 @@
+# Read Me First
+通过反问 http://localhost:8080/actuator 及子节点获取程序相关信息
---
+++ core/Actuator/actuator-base/src/main/java/demo/ActuatorBaseApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorBaseApplication.class, args);
+    }
+
+}
